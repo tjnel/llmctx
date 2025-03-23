c1ff62d0f118dec03c2dddf9a3cc9c9ecb862cd6
TJ Nel <mikiozen@gmail.com>
2025-03-23 01:52:42 -0500

    run13

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,9 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
 			// Explicitly specify Node.js 18 as the runtime
-			runtime: 'nodejs18.x'
+			runtime: 'nodejs18.x',
+			// Set bypassToken for prerender validation
+			bypassToken: 'c6a07c8e9ad8b8f0e6a5c5f4d3b2a1e0d9c8b7a6d5e4f3c2b1a0p1o2i3u4y5t6r7e8w9q0'
 		})
 	}
 }
